5a50263a250622a971b53ba27d7ec8a4e251f0bb
cbluebob <chudzinski.ludovic@gmail.com>
2026-09-23 11:15:00 +0200

    Expose orchestration API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from "./core/types";
 export * from "./core/mission-engine";
+export * from "./core/orchestrator";
+export * from "./core/planner";
 export * from "./guard/policy";
 export * from "./memory/journal";
