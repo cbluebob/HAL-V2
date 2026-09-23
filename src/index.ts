d6dca459db429946a580f864ebcfca84d27ac280
cbluebob <chudzinski.ludovic@gmail.com>
2026-09-23 11:31:59 +0200

    Expose persistent autonomy policy

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./core/mission-engine";
 export * from "./core/orchestrator";
 export * from "./core/planner";
 export * from "./core/autonomy";
+export * from "./core/autonomy-policy";
 export * from "./guard/policy";
 export * from "./memory/journal";
 export * from "./tools";
